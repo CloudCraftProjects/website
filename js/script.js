ede9004176c8eaec18e7293830504132348aec0e
booky10 <github@booky.tk>
2020-12-07 18:48:23 +0100

    added basic files

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function writeCopyright() {
+    document.write(
+        "<p class=\"m-0 text-center text-white\">" +
+            "&copy; " + new Date().getFullYear() + " CloudCraft, made by booky10." +
+        "</p>"
+    );
+}
